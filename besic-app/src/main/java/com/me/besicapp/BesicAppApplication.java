a2c75c72db94a48b9c7e2926b87e8d5bf59773e4
heinrichp94 <heinrich94p@gmail.com>
2023-08-09 20:24:28 +0200

    initial commit

---
+++ besic-app/src/main/java/com/me/besicapp/BesicAppApplication.java
@@ -0,0 +1,13 @@
+package com.me.besicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BesicAppApplication.class, args);
+    }
+
+}
